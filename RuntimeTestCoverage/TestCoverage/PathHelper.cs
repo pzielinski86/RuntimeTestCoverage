9a330bda9589a4c3041ad686e3c3e3d7e4138cf0
pzielinski86 <contact@pzielinski.com>
2015-11-22 19:49:38 +0000

    Storing  all temporary data in one folder

--- RuntimeTestCoverage/TestCoverage/PathHelper.cs
+++ RuntimeTestCoverage/TestCoverage/PathHelper.cs
@@ -5,11 +5,6 @@ namespace TestCoverage
 {
     public static class PathHelper
     {
-        public static string GetRewrittenFilePath(string documentPath)
-        {
-            return documentPath + ".coverage";
-        }
-
         public static string GetCoverageDllName(string assemblyName)
         {
             if (assemblyName.EndsWith("_COVERAGE.dll"))
